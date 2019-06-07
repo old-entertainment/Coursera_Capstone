39e5cd1d6fcb74157bee3cd72155e101f088b205
Igor Grigorev <barsic1080@gmail.com>
2019-06-08 01:30:03 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module with all data manipulations
+   P.S. Can be converted to .ipynb"""
+
+# %%
+## It is so good to be here
+
+# %%
+# all the imports
+import pandas as pd
+import numpy
+
+# %%
+# lets print
+print('Hello Capstone Project Course!')
